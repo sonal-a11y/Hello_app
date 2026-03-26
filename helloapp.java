fd281cc23c771a349df5816ffdc3315826c09638
os1378 <os1378@srmist.edu.in>
2026-03-26 09:11:45 +0530

    added UC2

--- helloapp.java
+++ helloapp.java
@@ -1,5 +1,16 @@
-public class helloapp {
+/*public class helloapp {
 public static void main (String args[]) {
 System.out.println("hello world");
 }
+}
+*/
+public class helloapp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
 }
