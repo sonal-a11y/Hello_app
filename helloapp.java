44c6b25d0256623d4e42c1b75ae985f482e0dc70
os1378 <os1378@srmist.edu.in>
2026-04-06 02:02:24 +0530

    added UC4

--- helloapp.java
+++ helloapp.java
@@ -14,7 +14,7 @@ public class helloapp {
         }
     }
 }
-*/
+
 public class helloapp {
     public static void main(String[] args) {
         String name;
@@ -28,5 +28,20 @@ public class helloapp {
 
         // Display greeting
         System.out.println("Hello " + name);
+    }
+}
+*/
+public class helloapp {
+    public static void main(String[] args) {
+
+        // If no arguments → default
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            // Loop through all names
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        }
     }
 }
